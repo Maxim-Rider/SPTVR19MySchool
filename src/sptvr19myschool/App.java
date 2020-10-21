9d1f9bb8f2a6a6646f274bcc883110c14d54e9df
Maksim Kolesnikov <maksim.kolesnikov@ivkhk.ee>
2020-10-21 10:28:34 +0300

    Start

---
+++ src/sptvr19myschool/App.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sptvr19myschool;
+
+/**
+ *
+ * @author Comp
+ */
+class App {
+    public void run() {
+        System.out.println("---Моя школа---");
+    }
+}
